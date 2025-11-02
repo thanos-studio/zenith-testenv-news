6067b399c6ab5da33cd59aa50c93ab77bb4b8aad
selwjoo <moi080917@gmail.com>
2025-11-02 21:34:10 +0900

    언론사 구독도 만듦

--- News/Main/views.py
+++ News/Main/views.py
@@ -10,12 +10,6 @@ def press_list(request):
     articles = Article.objects.all().order_by('-pub_date')  # 최신 순
     return render(request, 'press_list.html', {'presses': presses, 'articles': articles})
 
-def subscribe(request, press_id):
-    press = get_object_or_404(Press, id=press_id)
-    # 구독 로직 구현 (예: request.user가 구독 리스트에 추가)
-    # 예시: request.user.subscriptions.add(press)
-    return redirect('Main:press_list')  # 구독 후 메인 페이지로 돌아가기
-
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     return render(request, 'article_detail.html', {'article': article})
@@ -29,3 +23,12 @@ def article_create(request):
     else:
         form = ArticleForm()
     return render(request, 'article_form.html', {'form': form})
+
+def subscribe(request, press_id):
+    press = get_object_or_404(Press, id=press_id)
+    if request.user.is_authenticated:
+        if request.user in press.subscribers.all():
+            press.subscribers.remove(request.user)  # 이미 구독하면 취소
+        else:
+            press.subscribers.add(request.user)     # 구독 추가
+    return redirect('Main:press_list')  # 다시 돌아가기
