f46c4a55e9c7c32126e1c92adfe07a61911a3237
selwjoo <moi080917@gmail.com>
2025-11-02 18:10:21 +0900

    기사 사진 필요

--- News/Main/admin.py
+++ News/Main/admin.py
@@ -3,11 +3,11 @@ from .models import Press, Article
 
 @admin.register(Press)
 class PressAdmin(admin.ModelAdmin):
-    list_display = ('name', 'logo')  # 목록에서 보여줄 필드
-    search_fields = ('name',)        # 검색 가능
+    list_display = ('name', 'logo')
+    search_fields = ('name',)
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'press', 'pub_date')
-    list_filter = ('press', 'pub_date')  # 필터 기능
+    list_display = ('title', 'press', 'pub_date', 'image')
+    list_filter = ('press', 'pub_date')
     search_fields = ('title', 'content')
